2748be8e62b889b0549ab75fcd0eb9b889b9fedb
apostolov-ivan <tom.8707e@gmail.com>
2025-05-26 16:37:27 +0300

    changed

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 STUDENT_ID = "Apostolov_47b86c25"
 SOURCES    = [
-    "https://www.bbc.com/ukrainian/rss.xml",
-    "https://www.ukrinform.ua/rss",
-    "http://feeds.reuters.com/reuters/topNews"
+    "https://www.bbc.com/ukrainian/rss",
+    "http://feeds.reuters.com/reuters/topNews",
+    "https://feeds.bbci.co.uk/news/world/rss.xml",
+    "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+
+
 ]
     
